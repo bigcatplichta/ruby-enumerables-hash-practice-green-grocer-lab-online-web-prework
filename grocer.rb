ee761e91c79bf1b39adc4bd5ce975a7a9d520b33
Matt Plichta <bigcatplichta@gmail.com>
2019-08-26 18:34:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -25,8 +25,8 @@ def apply_coupons(cart, coupons)
   
   coupons.each do |coupon|
     item = coupon[:item]
-    if cart[item] && cart[item][:count] >= coupon[:num] && !cart[]
-    
+    if cart[item] && cart[item][:count] >= coupon[:num] # && !cart[]
+      cart["#{item} W/COUPON"] 
  
 end
 
