4d1da3c6dbee3026501a273e88f5d27137499d05
Matt Plichta <bigcatplichta@gmail.com>
2019-08-26 16:25:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -3,10 +3,11 @@ def consolidate_cart(cart)
   
   cart.each do |item|
       item_name = item.keys
+      attributes = item.values
         if final_cart[item_name]
           final_cart[item_name][:count] += 1
         else 
-          final_cart = item_name(item_name.values)
+          final_cart = item_name[attributes]
           final_cart[item_name][:count] = 1
         end 
   end 
