82b7528bb20531f07df621caed5439e6a776e704
Matt Plichta <bigcatplichta@gmail.com>
2019-08-26 19:06:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,7 +36,7 @@ end
 def apply_clearance(cart)
   cart.collect do |name, attributes|
     if attributes[:clearance]
-      attributes[:price] = attributes[price] * 0.8
+      attributes[:price] = attributes[:price] * 0.8
     end   
   end 
 end
